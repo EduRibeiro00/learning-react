c913af4c26f9c36e12866f44a706b1c22d6ac672
EduRibeiro00 <eribeiro306@gmail.com>
2020-01-29 17:15:09 +0000

    Changed style of completed items

--- src/components/MyTodoItem.js
+++ src/components/MyTodoItem.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class MyTodoItem extends Component {
     render() {
         return (
-            <div className="todo-item">
+            <div className={this.props.task.completed ? "todo-item completed" : "todo-item"}>
                 <input type="checkbox" 
                     checked={this.props.task.completed} 
                     onChange={() => this.props.handleCheckboxClick(this.props.task.id)}
