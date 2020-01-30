0e17e7e12d4e59c856d5ef76a6eb1b55ac0de84b
EduRibeiro00 <eribeiro306@gmail.com>
2020-01-30 15:28:52 +0000

    Can click on the whole to do item

--- src/components/MyTodoItem.js
+++ src/components/MyTodoItem.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function MyTodoItem({task, handleClick}) {
     return (
-        <div className={task.completed ? "todo-item completed" : "todo-item"}>
+        <div 
+            className={task.completed ? "todo-item completed" : "todo-item"}
+            onClick={() => handleClick(task.id)}
+        >
             <input type="checkbox" 
-                checked={task.completed} 
-                onChange={() => handleClick(task.id)}
+                checked={task.completed}
             />
             <p>{task.name}</p>
         </div>
