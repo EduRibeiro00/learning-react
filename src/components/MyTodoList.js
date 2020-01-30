52fe73562f6948b2018b6dc7ee4096a5f3049705
EduRibeiro00 <eribeiro306@gmail.com>
2020-01-30 14:48:46 +0000

    Refactor on the checkbox function

--- src/components/MyTodoList.js
+++ src/components/MyTodoList.js
@@ -2,31 +2,31 @@ import React, { useState } from 'react'
 import MyTodoItem from './MyTodoItem.js'
 import tasksList from '../data/tasksList.js'
 
-function handleCheckboxClick(setTasks, id) {
-  setTasks(prevTasks => {
-    const newTasksList = prevTasks.map(taskItem => {
-      if (taskItem.id === id) {
-        return {
-          ...taskItem,
-          completed : !taskItem.completed
-        }
-      }
-      return taskItem
-    })
-    
-    return newTasksList
-  })
-}
-
 export default function MyTodoList() {
   const [tasks, setTasks] = useState(tasksList)
+
+  const handleCheckboxClick = id => {
+    setTasks(prevTasks => {
+      const newTasksList = prevTasks.map(taskItem => {
+        if (taskItem.id === id) {
+          return {
+            ...taskItem,
+            completed : !taskItem.completed
+          }
+        }
+        return taskItem
+      })
+      
+      return newTasksList
+    })
+  }
 
   const taskComponent = tasks.map(item => {
       return (
         <MyTodoItem 
           key={item.id} 
           task={item} 
-          handleCheckboxClick={handleCheckboxClick.bind(this, setTasks)}
+          handleCheckboxClick={handleCheckboxClick}
         />
       )
     }
